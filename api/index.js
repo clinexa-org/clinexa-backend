9af619045f62c7303789bae1ab32b8d67682d16e
FARES SAMY <109601304+fares1908@users.noreply.github.com>
2026-01-16 15:43:15 +0200

    Fix: Initialize DB connection in serverless entry point

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import app from "../src/app.js";
+import connectDB from "../src/config/db.js";
+
+// Connect to database for serverless function
+connectDB();
 
 export default app;
+
